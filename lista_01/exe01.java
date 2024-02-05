d43a78489f5be59d371ab2c513dd88c3f1224113
Filipe Haniel <filipe.haniel@gmail.com>
2024-02-04 22:57:47 -0300

    fixing first problem

--- lista_01/exe01.java
+++ lista_01/exe01.java
@@ -1,5 +1,14 @@
-class exe01 {
+class Exe01 {
     public static void main(String[] args) {
-        System.out.println("Oi Mundo!!");
+
+        double theSum = sum(2, 3);
+
+        System.out.println("The sum of two numbers is: " + theSum);
+    }
+
+    static double sum(double numA, double numB) {
+        double sumOfTwoNumbers = numA + numB;
+
+        return sumOfTwoNumbers;
     }
 }
